a7b00575a25c2620c60adfd829177e8ee6775a56
Juan Carlos Perez Zapata <35706696+jcperezz@users.noreply.github.com>
2021-11-25 09:57:48 -0500

    code cleanup

--- lib/providers/local_storage_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class SharedPreferencesProvider {
-  static late SharedPreferences prefs;
-
-  static Future<void> init() async {
-    prefs = await SharedPreferences.getInstance();
-  }
-}
